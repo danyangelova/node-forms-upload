8401461e9d9a3db770073e1113c960b435918614
danyangelova <daniela.ant.ang@gmail.com>
2025-11-23 21:20:33 +0200

    Inspect multipart body parts
    • uploading the form (enctype="multipart/form-data)
    • added new type file input
    
    • the incoming data is different now, now each chunk is a Buffer
    • concatenated all chunks into one Buffer and converted to binary string
    • extracted boundary from Content-Type and split the raw body into parts

--- index.js
+++ index.js
@@ -21,14 +21,18 @@ server.on("request", (req, res) => { //"request" event passes req and res to the
     <title>Node.js form</title>
 </head>
 <body>
-    <form action="/" method="POST">
+    <form action="/upload" method="POST" enctype="multipart/form-data"> <!-- html obiknovena forma ne moje da izprati image -->
         <div>
             <label for="username">Username</label>
             <input type="text" name="username" id="username">
         </div>
         <div>
             <label for="password">Password</label>
-            <input type="text" name="password" id="password">
+            <input type="password" name="password" id="password">
+        </div>
+        <div>
+            <label for="avatar">Avatar</label>
+            <input type="file" name="avatar" id="avatar">
         </div>
         <div>
             <input type="submit" value="Send">
@@ -43,16 +47,37 @@ server.on("request", (req, res) => { //"request" event passes req and res to the
         console.log('----POST DATA----');
         let body = "";
         req.on("data", chunk => {
-            console.log(`chunk: ${chunk}`); //query string expecting //cqlata forma ni idva kato edin string
+            // console.log(`chunk: ${chunk}`); //query string expecting //cqlata forma ni idva kato edin string
             body += chunk;
         });
-
         req.on("end", () => {
             // console.log(body); //the query string
             const data = queryString.parse(body); //query string -> js object
-            console.log(data);
+            // console.log(data);
             res.end();
         });
+
+    } else if (req.url === "/upload" && req.method === "POST") {
+        console.log('----POST DATA 2----');
+
+        const body = [];
+        req.on("data", chunk => { //every chunk is a Buffer containing raw multipart data (boundary, headers, text, binary)
+            body.push(chunk);
+        });
+
+        req.on("end", () => {
+            const fullBody = Buffer.concat(body); //in one Buffer
+            const data = fullBody.toString("binary"); //default: utf8 - to moje da decode-ne greshno binary baitovete; "binary" - dai mi baitovete kato 8 bitovi binarni stoinosti
+            const boundary = req.headers['content-type'].split("boundary=")[1];
+
+            const parts = data.split(`--${boundary}`); //because --<boundary>
+            console.log(parts[1]);
+            console.log(parts[2]);
+            // console.log(parts[3]); //img
+            
+            res.end();
+        });
+
     }
 
 
